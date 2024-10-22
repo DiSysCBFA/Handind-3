c0b9bf0604a72e66e2bc2fb33781a320ee0b1140
polu <polu@itu.dk>
2024-10-22 12:53:52 +0200

    added a getclock function

--- service/clock.go
+++ service/clock.go
@@ -8,9 +8,16 @@ func (LcClock LamportClock) AddClock(username string) {
 	LcClock[username] = 0
 }
 
-// increment the clock of the given username
+// Increment the clock of the given username
 func (LcClock LamportClock) Tick(username string) {
 
 	// TODO: implement check for if the username exists in the map. otherwise create it.
 	LcClock[username]++
+}
+
+// Get the clock of the given username
+
+func (LcClock LamportClock) GetClock(username string) int {
+
+	return LcClock[username]
 }
