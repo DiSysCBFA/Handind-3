3d6065fbecb24274317e0eb4ba4f0a286249567a
polu <polu@itu.dk>
2024-10-22 13:08:30 +0200

    created clock for username

--- client/main.go
+++ client/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+
+	"../service"
 )
 
 var (
 	username string
+	LcClock  service.LamportClock
 )
 
 func main() {
 
+	// Init username
 	fmt.Println("Enter username:")
 	fmt.Scan(&username)
 	fmt.Println("Hello", username)
+
+	// Init clock on username
+	LcClock.AddClock(username)
+
 }
