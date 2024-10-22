62f93b0bac82e642119b00ff9ba17cf7cef7af0f
polu <polu@itu.dk>
2024-10-22 12:49:28 +0200

    Added init on lamport clock

--- service/clock.go
+++ service/clock.go
@@ -3,6 +3,11 @@ package main
 // Lapart clock type with a map of username (string) to each individual clock (int)
 type LamportClock map[string]int
 
+// Initialize a new LamportClock on username
+func (LcClock LamportClock) AddClock(username string) {
+	LcClock[username] = 0
+}
+
 // increment the clock of the given username
 func (LcClock LamportClock) Tick(username string) {
 
