01d3fbd335a3d5f849c82a554f27716a305be04b
polu <polu@itu.dk>
2024-10-27 11:46:18 +0100

    added setters to message

--- client/message.go
+++ client/message.go
@@ -14,3 +14,16 @@ type Message struct {
 func (m *Message) display() {
 	fmt.Println(m.username, "@ ", m.timestamp, "says: ", m.content)
 }
+
+func (m *Message) setUsername(username string) {
+	m.username = username
+}
+
+func (m *Message) setContent(content string) {
+	m.content = content
+
+}
+
+func (m *Message) setTimestamp() {
+	m.timestamp = time.Now()
+}
