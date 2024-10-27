630cfb15ef6630cac07b9fc7588ea76843a76a4c
polu <polu@itu.dk>
2024-10-27 12:35:26 +0100

    added some functions to server

--- server/server.go
+++ server/server.go
@@ -2,8 +2,20 @@ package main
 
 import "handin-3/service"
 
+const (
+	name = "Chitty-Chat-Server"
+)
+
 type server struct {
 	clock service.LamportClock
 
 	name string
+}
+
+func (s *server) incrementClock() {
+	s.clock.Tick(s.name)
+}
+
+func (s *server) getName() string {
+	return s.name
 }
