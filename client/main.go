b8c798ee54083bd78b89ec832e90c7660ac3ca41
cslj <cslj@itu.dk>
2024-10-28 16:04:36 +0100

    .

--- client/main.go
+++ client/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/DiSysCBFA/Handind-3/service"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultAdress = "localhost:5000"
+	name          = "chittyChat"
 )
 
 var (
 	username string
+	adrress  string
 	LcClock  service.LamportClock
+	client   service.ChittyChatClient
 )
 
 func main() {
@@ -26,19 +39,80 @@ func main() {
 }
 
 func StartClient(NameInput string, adressInput string) {
-
-	
-}
-
-func JoinChat(){
-
-}
-func LeaveChat(){
-
-}
-
-func sendMessage(){
+	// Connect to server
+	conn, err := grpc.Dial(adressInput, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	/*c := api.NewChatServiceClient(conn)
+	JoinChat(c)
+	//go broadcastListener(c)*/
 
 }
 
-func ListnerforMessages(){}
+func JoinChat() {
+
+	log.Printf("Joining chat as user: %s on time %d...", username, LcClock.GetClock(name))
+
+	// Create a context with a timeout to avoid indefinite waits
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	/*res, err := client.Join(ctx, &service.Joins{
+	      Participant: username,
+	      Timestamp:   int32(LcClock.GetClock(username)),
+	  })
+
+	  if err != nil {
+	      log.Fatalf("could not join chat: %v", err)
+	  }*/
+
+	LcClock.Tick(username)
+	log.Printf("Welcome! You just joined the chat with status: %s at time %d", res.Participant, LcClock.GetClock(username))
+}
+func LeaveChat() {
+
+	log.Printf("Leaving chat as user: %s on time %d...", username, LcClock.GetClock(username))
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	/* _, err := client.Leave(ctx, &service.Leaves{
+	       Participant: username,
+	   })
+	   if err != nil {
+	       log.Fatalf("Could not leave chat: %v", err)
+	   }*/
+
+}
+
+func sendMessage(content string) {
+	LcClock.Tick(username)
+	log.Printf("Sending message as user: %s on time %d...", username, LcClock.GetClock(username))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	/* _, err := client.Toserver(ctx, &service.Message{
+	       Participant: username,
+	   })
+	   if err != nil {
+	       log.Fatalf("Could not send message: %v", err)
+	   }*/
+}
+
+func listenerforMessages() {
+
+	/* stream, err := client.Broadcast(context.Background(), &service.Message{})
+	   if err != nil {
+	       log.Fatalf("Error listening for messages: %v", err)
+	   }
+
+	   for {
+	       msg, err := stream.Recv()
+	       if err != nil {
+	           log.Fatalf("Error receiving message: %v", err)
+	       }
+	       fmt.Printf("Received message from %s: %s\n", msg.Participant, msg.Content)
+	   }*/
+}
