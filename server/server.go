77ea9330a1d58d2d77de17da1b3e7e293573970b
polu <polu@itu.dk>
2024-10-28 18:23:45 +0100

    it has started to slowly come together

--- server/server.go
+++ server/server.go
@@ -5,88 +5,98 @@ import (
 	"log"
 
 	"github.com/DiSysCBFA/Handind-3/service"
-
 	"google.golang.org/grpc"
 
 	tasks "github.com/DiSysCBFA/Handind-3/api"
 )
 
 type server struct {
-	tasks.UnimplementedChittyChatServer
-	clock service.LamportClock
-
-	users map[string]tasks.ChittyChat_BroadcastServer
-
-	name string
+	tasks.UnimplementedChittyChatServer // Embedding the unimplemented server for forward compatibility
+	clock                               service.LamportClock
+	users                               map[string]tasks.ChittyChat_BroadcastServer
+	name                                string
 }
 
+// Initialize the Lamport clock for the server instance
 func (s *server) init() {
-	// Init clock on server
 	s.clock.AddClock(s.name)
 }
 
+// Method to increment the clock for the server
 func (s *server) incrementClock() {
 	s.clock.Tick(s.name)
 }
 
+// Method to determine the new clock based on a sender's clock
 func (s *server) determineNewClock(sender string) {
 	s.clock.DetermineNewClock(sender, s.name)
 }
 
+// Retrieve the server's name
 func (s *server) getName() string {
 	return s.name
 }
 
+// Method for handling a new member joining the chat
 func (s *server) memberJoin(member string) error {
 	s.users[member] = nil
 	log.Printf("User %s joined the chat", member)
 	return nil
 }
 
+// Retrieve the current clock for the server as an int32
 func (s *server) GetClock() int32 {
 	return int32(s.clock.GetClock(s.getName()))
 }
 
+// Create and initialize a new server instance
 func CreateServer(name string) (*server, error) {
-
-	chittyChatServer := server{
+	chittyChatServer := &server{
 		clock: service.LamportClock{},
 		name:  name,
+		users: make(map[string]tasks.ChittyChat_BroadcastServer),
 	}
-
-	return &chittyChatServer, nil
+	chittyChatServer.init()
+	return chittyChatServer, nil
 }
 
+// Create a new gRPC server instance
 func CreateGrpcServer(name string) (*grpc.Server, error) {
 	grpcServer := grpc.NewServer()
 	chatServer, err := CreateServer(name)
-	chatServer.init()
-
 	if err != nil {
 		return nil, err
 	}
 
-	tasks.RegisterChittyChatServer(grpcServer, *chatServer)
+	// Register the ChittyChat server
+	tasks.RegisterChittyChatServer(grpcServer, chatServer)
 
 	log.Printf("Starting gRPC server with name: %s", name)
 
 	chatServer.incrementClock()
 
 	return grpcServer, nil
-
 }
 
-func (s server) Join(context context.Context, req *tasks.JoinRequest) error {
-	log.Printf("a client wants to join the chat")
+// Implementation of the Join method as defined in the ChittyChatServer interface
+func (s *server) Join(ctx context.Context, req *tasks.Joins) (*tasks.JoinMessage, error) {
+	log.Printf("A client wants to join the chat")
 
-	// add the client to the broadcast
-	err := s.memberJoin(req.GetUsername())
+	// Add the client to the broadcast
+	err := s.memberJoin(req.GetParticipant())
 	if err != nil {
-		return err
+		return nil, err
 	}
-	// if the client is not in the clients map, add it to the clients map
-	// and return a JoinResponse with a status of OK
-	log.Printf("[%s: %d] Received a JOIN req from node %s", s.getName(), s.GetClock())
+
+	// Log the join request
+	log.Printf("[%s: %d] Received a JOIN request from: %s", s.getName(), s.GetClock(), req.GetParticipant())
+
+	// Increment the clock after processing the request
 	s.incrementClock()
-	return nil
+
+	// Construct and return the JoinMessage with the current Lamport clock
+	return &tasks.JoinMessage{
+		Participant: req.GetParticipant(),
+		Timestamp:   s.GetClock(),
+	}, nil
 }
