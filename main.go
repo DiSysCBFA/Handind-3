51b1cfa530463b3f9923728c921e65f87bdc58e9
polu <polu@itu.dk>
2024-10-28 10:32:47 +0100

    added more to start ui. Added enter adress and username
    Co-Authored-By: Caroline <cslj@itu.dk>

--- main.go
+++ main.go
@@ -20,14 +20,47 @@ func main() {
 
 	if result == "Start Server" {
 		//start new server
+
+		//use grpc to start new server
 	}
 
 	if result == "Start new Client" {
 		//start new client
+
+		selectionName := promptui.Select{
+			Label: "Input desired name, or leave blank for defualt",
+		}
+
+		_, resultName, err := selectionName.Run()
+
+		if err != nil {
+			log.Fatalf("Failed to run: %v", err)
+		}
+
+		if resultName == "" {
+			resultName = "Client"
+		}
+
+		selectionAddress := promptui.Select{
+			Label: "Input desired address, or leave blank for defualt",
+		}
+
+		_, resultAddress, err := selectionAddress.Run()
+
+		if err != nil {
+			log.Fatalf("Failed to run: %v", err)
+		}
+
+		// use grpc to start new client
+
 	}
 
 	if result == "Exit" {
+
 		//exit
+
+		log.Println("Exiting...")
+
 	}
 
 }
