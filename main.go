8ca2fadd77e100eb9927d456549671df571899d8
polu <polu@itu.dk>
2024-11-12 11:35:05 +0100

    server starts

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
 
 	"github.com/DiSysCBFA/Handind-3/server"
-
-	"github.com/DiSysCBFA/Handind-3/client"
 
 	"github.com/manifoldco/promptui"
 )
@@ -26,41 +25,74 @@ func main() {
 	if result == "Start Server" {
 		//start new server
 
-		server.SetupServer()
+		SetupServer()
 
 		//use grpc to start new server
 	}
+	/*
 
-	if result == "Start new Client" {
-		// Prompt for client name
-		selectionName := promptui.Prompt{
-			Label: "Enter desired name",
-		}
-		username, err := selectionName.Run()
-		if err != nil {
-			log.Fatalf("Failed to run: %v", err)
-		}
+		if result == "Start new Client" {
+			// Prompt for client name
+			selectionName := promptui.Prompt{
+				Label: "Enter desired name",
+			}
+			username, err := selectionName.Run()
+			if err != nil {
+				log.Fatalf("Failed to run: %v", err)
+			}
 
-		// Prompt for server address without a default
-		selectionAddress := promptui.Prompt{
-			Label: "Enter server address",
+			// Prompt for server address without a default
+			selectionAddress := promptui.Prompt{
+				Label: "Enter server address",
+			}
+			address, err := selectionAddress.Run()
+			if err != nil || address == "" {
+				log.Fatalf("Address must be provided")
+			}
+			address = "localhost:" + address
+			client.StartClient(username, address)
+			select {
+			}
+
+
 		}
-		address, err := selectionAddress.Run()
-		if err != nil || address == "" {
-			log.Fatalf("Address must be provided")
-		}
-		address = "localhost:" + address;
-		client.StartClient(username, address)
-		select {
-		// Listen for messages
-		//listenerforMessages()
-		}
-	}
+	*/
 
 	if result == "Exit" {
 
 		//exit
 		log.Println("Exiting...")
 		os.Exit(1)
+	}
+}
+
+const (
+	port = ":4002"
+	name = "ChittyChat"
+)
+
+func SetupServer() {
+	log.Println("Setting up server on port:", port, "with the name", name)
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer, err := server.CreateGrpcServer(name)
+	if err != nil {
+		log.Fatalf("Failed to create gRPC server: %v", err)
+	}
+
+	defer func() {
+		if err := lis.Close(); err != nil {
+			log.Fatalf("Failed to close listener: %v", err)
+		}
+	}()
+
+	log.Println("Server is now listening on port:", port)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
 	}
 }
--- server/server.go
+++ server/server.go
@@ -2,13 +2,46 @@ package server
 
 import (
 	"context"
+	"log"
 
 	chat "github.com/DiSysCBFA/Handind-3/api"
+	"github.com/DiSysCBFA/Handind-3/service"
 	"google.golang.org/grpc"
 )
 
-type Server struct {
+type server struct {
 	chat.UnimplementedChittyChatServer
+
+	name  string
+	clock service.LamportClock
+}
+
+// Create and initialize a new server instance
+func CreateServer(name string) (*server, error) {
+	chittyChatServer := &server{
+		clock: service.LamportClock{},
+		name:  name,
+	}
+	chittyChatServer.clock.AddClock(name)
+	return chittyChatServer, nil
+}
+
+// Create a new gRPC server instance
+func CreateGrpcServer(name string) (*grpc.Server, error) {
+	grpcServer := grpc.NewServer()
+	chatServer, err := CreateServer(name)
+	if err != nil {
+		return nil, err
+	}
+
+	// Register the ChittyChat server
+	chat.RegisterChittyChatServer(grpcServer, chatServer)
+
+	log.Printf("Starting gRPC server with name: %s", name)
+
+	chatServer.clock.Tick(chatServer.name)
+
+	return grpcServer, nil
 }
 
 func Broadcast(context.Context, *chat.Message) (*chat.Empty, error) {
